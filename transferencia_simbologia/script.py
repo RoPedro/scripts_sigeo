4fca4d26bee57e31edf7d27241d543d88192fcc4
Gabrielli Oliveira <gabrielli.osc@gmail.com>
2024-03-19 17:07:22 -0300

    feat: Create script to copy symbology

---
+++ transferencia_simbologia/script.py
@@ -0,0 +1,36 @@
+import ast
+import os
+from arcgis.gis import GIS
+from arcgis.features import FeatureLayer
+from dotenv import load_dotenv
+
+#Carrega o arquivo de variáveis de ambiente
+load_dotenv() 
+USER_NAME = os.getenv('APP_USER_NAME')
+PASSWORD = os.getenv('APP_PASSWORD')
+PORTAL = os.getenv('APP_PORTAL')
+WEBMAP = os.getenv('WEBMAP_ITEM')
+LAYER = os.getenv('LAYER_ITEM')
+INDEX = os.getenv('LAYER_INDEX')
+ 
+#Conexão com o arcgisonline
+gis = GIS(PORTAL, username=USER_NAME, password=PASSWORD)
+ 
+#Consulta os dados do webmap
+wm_item = gis.content.get(WEBMAP)
+webmap = wm_item.get_data()
+
+#Consulta a simbologia da camada no webmap
+operationalLayer = webmap['operationalLayers'][INDEX]
+symbol = operationalLayer['drawingInfo']['renderer']
+
+#Consulta a camada a partir do item
+lyr_item = gis.content.get(LAYER)
+layer = FeatureLayer.fromitem(lyr_item)
+
+#Tratamento de dados
+drawinginfo = '{"drawingInfo":{"renderer":' + str(symbol) + '}}'
+data_update = ast.literal_eval(drawinginfo)
+ 
+#Atualiza a camada com os dados de simbologia   
+layer.manager.update_definition(data_update)
